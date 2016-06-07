76343b47182f5fe209435bce907636cc71ed8983
Yurim Kang <kyr9909@naver.com>
2016-06-07 20:35:07 +0900

    #3 Event Handler

--- app/src/main/java/kr/hs/emirim/kyr9909/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/simpledialog/MainActivity.java
@@ -1,10 +1,13 @@
 package kr.hs.emirim.kyr9909.simpledialog;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -18,12 +21,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    public void onClick(View view) {
+    public void onClick(View v) {
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("First Dialog");
         dialog.setMessage("This is message part.");
         dialog.setIcon(R.drawable.first_icon);
-        dialog.setPositiveButton("확인",null);
+        dialog.setPositiveButton("확인",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int which){
+                Toast.makeText(MainActivity.this,"대화상자의 확인 버튼을 클릭했음.",Toast.LENGTH_LONG).show();
+            }
+        });
         dialog.show();
 
     }
