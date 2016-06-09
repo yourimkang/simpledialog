a4fd063184f06d31c552b12b848434284bb61d8c
Yurim Kang <kyr9909@naver.com>
2016-06-09 19:18:27 +0900

    #5 Change RadioButton List(Dialog)

--- app/src/main/java/kr/hs/emirim/kyr9909/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/kyr9909/simpledialog/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     String[] items={"젤리빈", "킷켓","롤리팝"};
+    boolean[] checkArr={false,true,false};
     Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +29,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("First Dialog");
-        dialog.setItems(items, new DialogInterface.OnClickListener(){
+        /*dialog.setItems(items, new DialogInterface.OnClickListener(){
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         but.setText(items[which]);
                     }
-                });
+                });*/
+
+        dialog.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialogInterface, int which) {
+                but.setText(items[which]);
+            }
+
+        });
+
+
         dialog.setIcon(R.drawable.first_icon);
         dialog.setPositiveButton("확인",new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int which){
